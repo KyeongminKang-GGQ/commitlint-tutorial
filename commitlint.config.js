06714fd540d9ed8abeb2d6c871c7a235cb9ce26f
KyeongminKang-GGQ <kyeongmin.kang@ggq.gg>
2022-11-29 17:53:00 +0900

    Fix: apply commit message type-enum

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,25 @@
-module.exports = { extends: ["@commitlint/config-conventional"] };
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-case": [2, "always", "pascal-case"],
+    "type-empty": [2, "never"],
+    "type-enum": [
+      2,
+      "always",
+      [
+        "Feature",
+        "Fix",
+        "Modify",
+        "Design",
+        "Style",
+        "Refactor",
+        "Comment",
+        "Docs",
+        "Test",
+        "Chore",
+        "Rename",
+        "Remove",
+      ],
+    ],
+  },
+};
